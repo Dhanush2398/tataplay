3bf67cb1d7d2e79deac8cb4db79f9997cb1bef45
dhanush <dhanush.dhoni007@gmail.com>
2025-06-12 12:58:39 +0530

    change

--- mail.php
+++ mail.php
@@ -8,17 +8,17 @@ $number = $_POST['number'] ?? '';
 
 $to = ""; 
 
-// Construct email
+
 $subject = "New contact from $name";
 $txt = "Name: $name\nPhone: $number\nEmail: $email\nMessage:\n$message";
 $headers = "From: $email";
 
-// Send email
+
 if (!empty($email) && !empty($message)) {
     mail($to, $subject, $txt, $headers);
 }
 
-// Redirect to thank you page
+
 header("Location: thankyou.html");
 exit;
 ?>
